fbecf36081857e92668034e8669759f6dbb2389d
PranavV110 <164237225+PranavV110@users.noreply.github.com>
2024-05-28 13:55:46 -0400

    Update frontend-1.py

--- frontend-1.py
+++ frontend-1.py
@@ -5,6 +5,7 @@ import pandas as pd
 @st.cache_data
 def load_data(file):
     df = pd.read_csv(file)
+    df = df.dropna()
     return df
 
 # Function to calculate breakdown of each value in a column
