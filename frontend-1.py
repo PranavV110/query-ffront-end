70cc444767f7faf6a760bf620d0dc5545db40c27
PranavV110 <164237225+PranavV110@users.noreply.github.com>
2024-05-28 20:11:41 -0400

    Update frontend-1.py

--- frontend-1.py
+++ frontend-1.py
@@ -5,7 +5,7 @@ import pandas as pd
 @st.cache_data
 def load_data(file):
     df = pd.read_csv(file)
-
+    df = df[df['pdf_link'].notna()]
     return df
 
 # Function to calculate breakdown of each value in a column
