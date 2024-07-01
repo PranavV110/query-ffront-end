bbcd047691a25296b2d5782205c91189f914faa1
PranavV110 <164237225+PranavV110@users.noreply.github.com>
2024-07-01 10:53:16 -0400

    Update frontend-1.py

--- frontend-1.py
+++ frontend-1.py
@@ -7,7 +7,7 @@ from fuzzywuzzy import fuzz, process
 @st.cache_data
 def load_data(file_path):
     df = pd.read_csv(file_path)
-    df['publication_date'] = pd.to_datetime(df['publication_date'], errors='coerce')
+    df['publication_date'] = pd.to_datetime(df['publication_date'])
     df.fillna('-', inplace=True)
     return df
 
