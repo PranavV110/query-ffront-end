d4c8e84bfc8f05f151622163f67553e867fc56f8
PranavV110 <164237225+PranavV110@users.noreply.github.com>
2024-08-19 12:21:39 -0400

    Update frontend-1.py

--- frontend-1.py
+++ frontend-1.py
@@ -32,7 +32,7 @@ def paginate_data(data, page, page_size):
 st.sidebar.title("Enter Search Parameters")
 
 # Specify the path to your CSV file
-csv_file_path = "test_df.csv"
+csv_file_path = "TEST_papers.csv"
 
 # Load data
 data = load_data(csv_file_path)
