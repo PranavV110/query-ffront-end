fce183d6768e681887a605bebaabac6bdde19365
PranavV110 <164237225+PranavV110@users.noreply.github.com>
2024-09-17 13:09:48 -0400

    Update frontend-1.py

--- frontend-1.py
+++ frontend-1.py
@@ -129,14 +129,14 @@ if 'filtered_data' in st.session_state:
     # Clean up special characters in the DataFrame
     filtered_data.replace({r'\n': ' ', r'\r': ' '}, regex=True, inplace=True)
 
-    filtered_data2 = filtered_data.astype(str)
+    filtered_data = filtered_data.astype(str)
 
     # Pagination controls
     page_size = 100
-    total_pages = (len(filtered_data2) + page_size - 1) // page_size  # Compute total pages
+    total_pages = (len(filtered_data) + page_size - 1) // page_size  # Compute total pages
     page = st.session_state['page']
 
-    paginated_data = paginate_data(filtered_data2, page, page_size)
+    paginated_data = paginate_data(filtered_data, page, page_size)
     #paginated_data['title'] = paginated_data['title'].astype(str)
 
     # Convert the DataFrame to HTML and style it
@@ -175,13 +175,13 @@ if 'filtered_data' in st.session_state:
 
     # Display number of results on the current page and total number of filtered results
     current_page_results = len(paginated_data)
-    total_results = len(filtered_data2)
+    total_results = len(filtered_data)
     start_idx = page * page_size + 1
     end_idx = start_idx + current_page_results - 1
     st.subheader(f"Showing {start_idx}-{end_idx} of {total_results} results")
 
     # Convert filtered data to CSV
-    csv = convert_df_to_csv(filtered_data2)
+    csv = convert_df_to_csv(filtered_data)
 
     # Download button on the sidebar
     st.sidebar.download_button(
